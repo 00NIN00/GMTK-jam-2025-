7c84acef0fc3a55fb9ccdd5628db6f0386613155
00nin00 <lego123lol@mail.ru>
2025-07-31 13:16:03 +0300

    add ecs

---
+++ Assets/Scripts/Ecs/EcsBootstrap.cs
@@ -0,0 +1,54 @@
+using Leopotam.Ecs;
+using UnityEngine;
+using Voody.UniLeo;
+
+public class EcsBootstrap : MonoBehaviour
+{
+    private EcsWorld _world;
+    private EcsSystems _systems;
+
+    private void Start()
+    {
+        _world =  new EcsWorld();
+        _systems = new EcsSystems(_world);
+
+        _systems.ConvertScene();
+        
+        AddInjections();
+        AddOneFrames();
+        AddSystems();
+        
+        _systems.Init();
+    }
+
+    private void AddInjections()
+    {
+        
+    }
+
+    private void AddSystems()
+    {
+        
+    }
+
+    private void AddOneFrames()
+    {
+        
+    }
+
+    private void Update()
+    {
+        _systems.Run();
+    }
+
+    private void OnDestroy()
+    {
+        if(_systems == null) return;
+        
+        _systems.Destroy();
+        _systems = null;
+        
+        _world.Destroy();
+        _world = null;
+    }
+}
